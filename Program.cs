50e58b1a298c86d6884510662eaa2dc8d0012bc3
Adrian Herreraalgotsson <Blazer1458@gmail.com>
2021-11-30 08:56:33 +0100

    highscore program

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random gen = new Random();
+            string filNamn = "highscore.txt";
+            string spelaIgen = "j";
+
+            //Läser in highscoret från textfilen och lägger det i en lista strings
+            List<string> highscoreList = File.ReadAllLines(filNamn).ToList();
+
+            //Lägger in highscoret i en lista av ints
+            List<int> intHighscoreList = new List<int>();
+            intHighscoreList.Add(int.Parse(highscoreList[0]));
+
+            while (spelaIgen == "j")
+            {
+                Console.Clear();
+
+                //Skriver ut det största talet i listan
+                Console.WriteLine($"Highscore på den här enheten är: {intHighscoreList.Max()}");
+
+                //Skapar ett slumptal
+                int num = gen.Next(1, 1001);
+
+                //Skriver ut slumptalet
+                if (num > intHighscoreList.Max())
+                {
+                    Console.WriteLine($"Nytt highscore!! Du fick {num}!");
+                }
+                else
+                {
+                    Console.WriteLine($"Du fick {num}\n");
+                }
+                //lägger in slumptalet i listan
+                intHighscoreList.Add(num);
+
+                //Frågar om man vill spela igen
+                Console.Write("\nVill du spela igen? (j/n) ");
+                spelaIgen = Console.ReadLine().ToLower();
+                if (spelaIgen == "n")
+                {
+                    break;
+                }
+            }
+            //lägger in det största talet i text filen
+            File.WriteAllText(filNamn, intHighscoreList.Max().ToString());
+        }
+    }
+}
